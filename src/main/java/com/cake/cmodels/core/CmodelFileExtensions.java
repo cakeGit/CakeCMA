eaaa4622ccf318566bcb040cc9d21f51bf44e250
cakeGit <65340665+cakeGit@users.noreply.github.com>
2024-04-10 13:57:06 +0100

    ✨ FINALLY MADE OBJ READING 🥳🥳🥳

--- src/main/java/com/cake/cmodels/core/CmodelFileExtensions.java
+++ src/main/java/com/cake/cmodels/core/CmodelFileExtensions.java
@@ -2,10 +2,10 @@ package com.cake.cmodels.core;
 
 public class CmodelFileExtensions {
 
-    // In the event that I get cancelled (or you don't want a dumbness filename), please change this to remove my name, c stands for compressed in that case
+    // In the event that I get cancelled (or you don't want a dumbass filename), change this, c stands for compressed in that case
     private static final boolean isCakeCancelled = false;
 
-    public static String SOURCE_DEFINITION_EXTENSION = isCakeCancelled ? ".cmodelsource.json" : ".cakesource.json";
-    public static String COMPILED_FILE_EXTENSION = isCakeCancelled ? ".cmodel" : ".cake";
+    public static String SOURCE_DEFINITION_EXTENSION = isCakeCancelled ? ".cmodelsource.json" : ".caksource.json";
+    public static String COMPILED_FILE_EXTENSION = isCakeCancelled ? ".cmodel" : ".cak";
 
 }
